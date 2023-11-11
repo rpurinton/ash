b273fe42c64060cee185a1ecbf47c06cb96327fb
Russell Purinton <russell.purinton@gmail.com>
2023-11-10 19:56:10 -0500

    Update Ash.php

--- src/Ash.php
+++ src/Ash.php
@@ -56,6 +56,7 @@ class Ash
             'user_id' => trim(shell_exec("whoami")),
             'working_dir' => trim(shell_exec("pwd")),
             'home_dir' => trim(shell_exec("echo ~")),
+            'ip_addr' => trim(shell_exec("ip addr | grep inet")),
         ];
         $this->sys_info['working_folder'] = basename($this->sys_info['working_dir'] == "" ? "/" : basename($this->sys_info['working_dir']));
         if ($this->debug) echo ("(ash) set_system_info() result: " . print_r($this->sys_info, true) . "\n");
