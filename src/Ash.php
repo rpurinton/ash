ff5519289a64557648ffad36b235b490f3e861e1
Russell Purinton <russell.purinton@gmail.com>
2023-11-10 20:39:20 -0500

    Update Ash.php

--- src/Ash.php
+++ src/Ash.php
@@ -23,6 +23,7 @@ class Ash
         echo "(ash) Installing dependencies...";
         $cmd = "cd " . __DIR__ . " && export COMPOSER_ALLOW_SUPERUSER=1 && export COMPOSER_NO_INTERACTION=1 && composer install 2>&1";
         exec($cmd, $output, $exit_code);
+        if ($this->debug) echo "(ash) install_dependencies() result: " . print_r($output, true) . "\n";
         if ($exit_code != 0) {
             echo "failed.\n";
             echo "(ash) Error: composer install failed.\n";
