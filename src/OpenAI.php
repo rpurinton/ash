f58c279c30f28f18dc85f4a57093ae5b1c66ac31
Russell Purinton <russell.purinton@gmail.com>
2023-11-10 23:25:29 -0500

    Update OpenAI.php

--- src/OpenAI.php
+++ src/OpenAI.php
@@ -14,7 +14,11 @@ class OpenAI
     {
         $this->client = \OpenAI::client($this->ash->config['openai_api_key']);
         $models = $this->client->models()->list();
-        foreach ($models as $model) if (substr($model['id'], 0, 3) == 'gpt') $this->models[] = $model['id'];
+        foreach ($models as $model) {
+            $model_id = $model['id'];
+            $model_family = substr($model_id, 0, 3);
+            if ($model_family == 'gpt') $this->models[] = $model_id;
+        }
         print_r($this->models);
     }
 }
