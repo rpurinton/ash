11cc25f49d5506794a9d7a35d7deb2f53b43b6a1
Russell Purinton <russell.purinton@gmail.com>
2023-11-10 23:10:39 -0500

    Update OpenAI.php

--- src/OpenAI.php
+++ src/OpenAI.php
@@ -7,9 +7,12 @@ require_once(__DIR__ . "/vendor/autoload.php");
 class OpenAI
 {
     private $client = null;
+    private $history = [];
 
     public function __construct(private $ash)
     {
         $this->client = \OpenAI::client($this->ash->config['openai_api_key']);
+        $models = $this->client->models();
+        print_r($models);
     }
 }
