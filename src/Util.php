f051f6f4c988b43fed98c43f4ea3dbb19a2d1a4a
Russell Purinton <russell.purinton@gmail.com>
2023-11-12 04:11:36 -0500

    Update Util.php

--- src/Util.php
+++ src/Util.php
@@ -30,19 +30,20 @@ class Util
 
     public function markdownToEscapeCodes($text, $color_support = true)
     {
-        $text = str_replace("\\e", "\e", $text);
         $text = str_replace("```", "", $text);
         if ($color_support) {
             $text = preg_replace('/\*\*(.*?)\*\*/', "\e[1m$1\e[0m", $text); // Bold
             $text = preg_replace('/\*(.*?)\*/', "\e[3m$1\e[0m", $text); // Italic
             $text = preg_replace('/_(.*?)_/', "\e[4m$1\e[0m", $text); // Underline
             $text = preg_replace('/`(.*?)`/', "\e[48;5;226m$1\e[0m", $text); // Highlight
+            $text = str_replace("\\e", "\e", $text);
             return $text;
         } else {
             $text = preg_replace('/\*\*(.*?)\*\*/', "$1", $text); // Bold
             $text = preg_replace('/\*(.*?)\*/', "$1", $text); // Italic
             $text = preg_replace('/_(.*?)_/', "$1", $text); // Underline
             $text = preg_replace('/`(.*?)`/', "$1", $text); // Highlight
+            $text = str_replace("\\e", "\e", $text);
             return $text;
         }
     }
