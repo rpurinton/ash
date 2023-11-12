5224cc03564b59680525a6e201e52c8ec05bae51
Russell Purinton <russell.purinton@gmail.com>
2023-11-12 02:25:58 -0500

    x

--- src/functions.d/write_file.php
+++ src/functions.d/write_file.php
@@ -2,18 +2,21 @@
 $this->functionHandlers['write_file'] = function ($args) {
     if ($this->ash->debug) echo ("debug: write_file(" . $args["path"] . ")\n");
     $path = $args['path'] ?? "";
-    $content = $args['content'] ?? null;
+    $content = $args['content'] ?? "";
     $append = $args['append'] ?? false;
-    $owner = $args['owner'] ?? null;
-    $group = $args['group'] ?? null;
-    $chmod = $args['chmod'] ?? null;
+    $owner = $args['owner'] ?? "";
+    $group = $args['group'] ?? "";
+    $chmod = $args['chmod'] ?? "";
     echo ("$path\n");
     if ($append) $result = file_put_contents($path, $content, FILE_APPEND);
     else $result = file_put_contents($path, $content);
-    if (!is_null($owner)) $own_result = chown($path, $owner) ? "yes" : "failed";
-    if (!is_null($group)) $grp_result = chgrp($path, $group) ? "yes" : "failed";
-    if (!is_null($chmod)) $mod_result = chmod($path, $chmod) ? "yes" : "failed";
+    if (($owner != "") && ($group != "")) {
+        shell_exec("chown $owner:$group $path");
+    }
+    if ($chmod != "") {
+        shell_exec("chmod $chmod $path");
+    }
     if ($this->ash->debug) echo ("debug: write_file() bytes written: $result\n");
-    $result = ["bytes_written" => $result, "owner_set" => $own_result, "group_set" => $grp_result, "chmod_set" => $mod_result];
+    $result = ["bytes_written" => $result];
     return $result;
 };
