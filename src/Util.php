4337d6dcff09b50d73bb459420f1b53061821d97
Russell Purinton <russell.purinton@gmail.com>
2023-11-12 04:39:30 -0500

    Update Util.php

--- src/Util.php
+++ src/Util.php
@@ -105,29 +105,38 @@ class Util
             "```zsh" => "Zsh Script:"
         ];
 
-        if ($colorSupport) {
-            // Handle nested markdown by replacing from the innermost to the outermost
-            $text = preg_replace('/(?<!\\\\)\\`(.*?)\\`/', "\e[7m$1\e[0m", $text); // Inline code
-            $text = preg_replace('/__(.*?)__/', "\e[4m$1\e[0m", $text); // Underline
-            $text = preg_replace('/\*\*(.*?)\*\*/s', "\e[1m$1\e[0m", $text); // Bold
-            $text = preg_replace('/\*(.*?)\*/s', "\e[3m$1\e[0m", $text); // Italic
-            $text = preg_replace('/~~(.*?)~~/', "\e[9m$1\e[0m", $text); // Strikethrough
-        } else {
-            $text = preg_replace('/__(.*?)__/', "$1", $text);
-            $text = preg_replace('/\*\*(.*?)\*\*/s', "$1", $text);
-            $text = preg_replace('/\*(.*?)\*/s', "$1", $text);
-            $text = preg_replace('/~~(.*?)~~/', "$1", $text);
-        }
+        $text = stripcslashes($text);
+        $text = str_replace(array_keys($replacements), array_values($replacements), $text);
+        $text = str_replace("```", "", $text);
 
+        if ($colorSupport) {
+            // look for text wrapped in **xxx**
+            $text = preg_replace("/\*\*(.*?)\*\*/", "\e[1m$1\e[0m", $text);
+            // look for text wrapped in *xxx*
+            $text = preg_replace("/\*(.*?)\*/", "\e[3m$1\e[0m", $text);
+            // look for text wrapped in _xxx_
+            $text = preg_replace("/\_(.*?)\_/", "\e[3m$1\e[0m", $text);
+            // look for text wrapped in ~xxx~
+            $text = preg_replace("/\~(.*?)\~/", "\e[9m$1\e[0m", $text);
+            // look for text wrapped in `xxx`
+            $text = preg_replace("/\`(.*?)\`/", "\e[7m$1\e[0m", $text);
+            return $text;
+        } else {
+            // strip out markdown characters
+            $text = preg_replace("/\*\*(.*?)\*\*/", "$1", $text);
+            $text = preg_replace("/\*(.*?)\*/", "$1", $text);
+            $text = preg_replace("/\_(.*?)\_/", "$1", $text);
+            $text = preg_replace("/\~(.*?)\~/", "$1", $text);
+            $text = preg_replace("/\`(.*?)\`/", "$1", $text);
+            return $text;
+        }
         // Handle URLs separately
         $text = preg_replace('/\[(.*?)\]\((.*?)\)/', "\e[34;4m$1\e[0m", $text);
 
         // Add any missing replacements here
-        $text = str_replace(array_keys($replacements), array_values($replacements), $text);
-        $text = str_replace("```", "", $text);
+
 
         // Fix escaped characters that should be treated literally
-        $text = stripcslashes($text);
         return $text;
     }
 }
