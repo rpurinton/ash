a8eec6986b20131f3fa1861276e5449a908a0945
Russell Purinton <russell.purinton@gmail.com>
2023-11-12 04:18:21 -0500

    Update Util.php

--- src/Util.php
+++ src/Util.php
@@ -30,21 +30,54 @@ class Util
 
     public function markdownToEscapeCodes($text, $color_support = true)
     {
-        $text = str_replace("```", "", $text);
+        $replacements = [
+            "```diff" => "Diff:",
+            "```yaml" => "YAML:",
+            "```json" => "JSON:",
+            "```bash" => "Bash Script:",
+            "```javascript" => "JavaScript:",
+            "```html" => "HTML:",
+            "```css" => "CSS:",
+            "```typescript" => "TypeScript:",
+            "```python" => "Python:",
+            "```ruby" => "Ruby:",
+            "```c" => "C:",
+            "```cpp" => "C++:",
+            "```csharp" => "C#:",
+            "```go" => "Go:",
+            "```java" => "Java:",
+            "```kotlin" => "Kotlin:",
+            "```rust" => "Rust:",
+            "```scala" => "Scala:",
+            "```swift" => "Swift:",
+            "```dart" => "Dart:",
+            "```elixir" => "Elixir:",
+            "```erlang" => "Erlang:",
+            "```haskell" => "Haskell:",
+            "```lisp" => "Lisp:",
+            "```lua" => "Lua:",
+            "```ocaml" => "OCaml:",
+            "```php" => "PHP:",
+            "```" => ""
+        ];
+        $text = str_replace(array_keys($replacements), array_values($replacements), $text);
+
         if ($color_support) {
             $text = preg_replace('/\*\*(.*?)\*\*/', "\e[1m$1\e[0m", $text); // Bold
             $text = preg_replace('/\*(.*?)\*/', "\e[3m$1\e[0m", $text); // Italic
             $text = preg_replace('/_(.*?)_/', "\e[4m$1\e[0m", $text); // Underline
             $text = preg_replace('/`(.*?)`/', "\e[48;5;226m$1\e[0m", $text); // Highlight
-            $text = str_replace("\\e", "\e", $text);
-            return $text;
         } else {
             $text = preg_replace('/\*\*(.*?)\*\*/', "$1", $text); // Bold
             $text = preg_replace('/\*(.*?)\*/', "$1", $text); // Italic
             $text = preg_replace('/_(.*?)_/', "$1", $text); // Underline
             $text = preg_replace('/`(.*?)`/', "$1", $text); // Highlight
-            $text = str_replace("\\e", "\e", $text);
-            return $text;
         }
+
+        // urls
+        $text = preg_replace('/\[(.*?)\]\((.*?)\)/', "\e[34;4m$1\e[0m", $text);
+        $text = str_replace("\\e", "\e", $text);
+
+        return $text;
     }
 }
