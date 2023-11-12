347214f8d7719e7160d788f548f19c3efb588bc3
Russell Purinton <russell.purinton@gmail.com>
2023-11-11 22:50:54 -0500

    Update linux_commands.php

--- src/functions.d/linux_commands.php
+++ src/functions.d/linux_commands.php
@@ -10,6 +10,7 @@ $this->functionHandlers['linux_commands'] = function ($args) {
         ];
         $pipes = [];
         try {
+            echo ("$ " . $input['command'] . "\n");
             $this->runningProcess = proc_open($input['command'], $descriptorspec, $pipes, $input['cwd'] ?? $this->ash->sysInfo->sysInfo['workingDir'], $input['env'] ?? []);
         } catch (\Exception $e) {
             return [
@@ -31,6 +32,7 @@ $this->functionHandlers['linux_commands'] = function ($args) {
                 "stderr" => $stderr,
                 "exitCode" => $exitCode,
             ];
+            echo ("$stdout\n$stderr\n");
             if ($this->ash->debug) echo ("debug: proc_exec() result: " . print_r($result, true) . "\n");
             return $result;
         }
