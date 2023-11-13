02013faf73ab77bcdfc916b1d048bf8802fbbfea
Russell Purinton <russell.purinton@gmail.com>
2023-11-13 10:55:48 -0500

    Update get_sys_info.php

--- src/functions.d/get_sys_info.php
+++ src/functions.d/get_sys_info.php
@@ -1,6 +1,7 @@
 <?php
 $this->functionHandlers['get_sys_info'] = function ($args) {
     if ($this->ash->debug) echo ("debug: get_sys_info()\n");
+    echo ("done!\n");
     $result = ["sys_info" => print_r($this->ash->sysInfo->sysInfo, true)];
     return $result;
 };
