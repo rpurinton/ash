9b9a1001eca3e99f417e0a2ba87d139cdc414d32
Russell Purinton <russell.purinton@gmail.com>
2023-11-14 05:58:29 -0500

    Update edit_file.php

--- src/functions.d/edit_file.php
+++ src/functions.d/edit_file.php
@@ -1,47 +1,28 @@
 <?php
 $this->functionHandlers['edit_file'] = function ($args) {
-    if ($this->ash->debug) echo ("debug: edit_file(" . print_r($args, true) . ")\n");
-    $sed_command = $args['sed_command'] ?? "";
-    echo ("$ " . $sed_command . "\n"); // display just the main argument
-    $stdOut = "";
-    $sdtErr = "";
+    $file_path = $args['file_path'] ?? null;
+    $sed_command = $args['sed_command'] ?? null;
+    $dry_run = $args['dry_run'] ?? false;
+    $stdOut = $stdErr = '';
     $exitCode = 0;
-    if (empty($sed_command)) {
-        $error = "Error (ash): Missing required fields.";
-        if ($this->ash->debug) echo ("debug: edit_file() error: $error\n");
+
+    if (!$file_path || !$sed_command) {
+        $error = "Error: 'file_path' and 'sed_command' are required.";
         return ["stdout" => "", "stderr" => $error, "exit_code" => -1];
     }
-    $command = $sed_command;
-    if ($this->ash->debug) echo ("debug: edit_file() command: $command\n");
-    $env = trim(shell_exec("env | grep -v '^BASH_FUNC_'"));
-    // parse it into key-value pairs
-    $env = explode("\n", $env);
-    $env = array_map(function ($item) {
-        $item = explode("=", $item);
-        if (isset($item[1])) return [$item[0] => $item[1]];
-        else return [];
-    }, $env);
-    $env = array_reduce($env, function ($carry, $item) {
-        return array_merge($carry, $item);
-    }, []);
-    $descriptorspec = array(
-        0 => array("pipe", "r"),  // stdin
-        1 => array("pipe", "w"),  // stdout
-        2 => array("pipe", "w")   // stderr
-    );
-    $process = proc_open($command, $descriptorspec, $pipes, null, $env);
-    if (is_resource($process)) {
-        $stdOut = stream_get_contents($pipes[1]);
-        fclose($pipes[1]);
-        $stdErr = stream_get_contents($pipes[2]);
-        fclose($pipes[2]);
-        $exitCode = proc_close($process);
+
+    echo ($file_path . "\n"); // display just the main argument
+
+    $command = "sed -i '' " . ($dry_run ? "-n 'p' " : "") . "'$sed_command' '$file_path'";
+    exec($command, $output, $exitCode);
+
+    if ($dry_run) {
+        $stdOut = implode("\n", $output);
+    } else {
+        $stdOut = $exitCode === 0 ? "File edited successfully." : "No changes made.";
     }
-    if ($stdOut == "" && $stdErr == "" && $exitCode == 0) {
-        $stdOut = "Clean exit, no output.";
-    }
-    if ($stdErr == "") $stdErr = $exitCode == 0 ? "No errors." : "Error (ash): Process exited with non-zero exit code.";
-    $result = ["stdout" => $stdOut, "stderr" => $sdtErr, "exit_code" => $exitCode];
-    if ($this->ash->debug) echo ("debug: read_file() result: " . print_r($result, true) . "\n");
-    return $result;
+
+    $stdErr = $exitCode === 0 ? "No errors." : "Error occurred with exit code: $exitCode";
+
+    return ["stdout" => $stdOut, "stderr" => $stdErr, "exit_code" => $exitCode];
 };
