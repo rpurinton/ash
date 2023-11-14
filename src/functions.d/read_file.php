ef40e5fa9796a551ddfe5af46ffa7283096334a1
Russell Purinton <russell.purinton@gmail.com>
2023-11-14 05:52:53 -0500

    Update read_file.php

--- src/functions.d/read_file.php
+++ src/functions.d/read_file.php
@@ -7,7 +7,16 @@ $this->functionHandlers['read_file'] = function ($args) {
         if ($this->ash->debug) echo ("debug: read_file() error: file not found: \"$path\"\n");
         return ["stdout" => "", "stderr" => "Error (ash): file not found: $path", "exit_code" => -1];
     }
-    $result = ["stdout" => file_get_contents($path), "stderr" => "", "exit_code" => 0];
+    $contents = file_get_contents($path);
+    if ($contents === false) {
+        if ($this->ash->debug) echo ("debug: read_file() error: file could not be read: \"$path\"\n");
+        return ["stdout" => "", "stderr" => "Error (ash): file could not be read: $path", "exit_code" => -1];
+    }
+    if ($contents = "") {
+        if ($this->ash->debug) echo ("debug: read_file() error: file is empty: \"$path\"\n");
+        return ["stdout" => "", "stderr" => "Error (ash): file is empty: $path", "exit_code" => -1];
+    }
+    $result = ["stdout" => $contents, "stderr" => "", "exit_code" => 0];
     if ($this->ash->debug) echo ("debug: read_file() result: " . print_r($result, true) . "\n");
     return $result;
 };
