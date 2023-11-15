ab4b1b56c580db16295827f866bfc56b3576cd94
Russell Purinton <russell.purinton@gmail.com>
2023-11-15 05:11:15 -0500

    x

--- src/functions.d/write_file.php
+++ src/functions.d/write_file.php
@@ -1,7 +1,12 @@
 <?php
 $this->functionHandlers['write_file'] = function ($args) {
-    if ($this->ash->debug) echo ("debug: write_file(" . $args["path"] . ")\n");
-    $path = $args['path'] ?? "";
+    if ($this->ash->debug) echo ("debug: write_file(" . $args["path"] ?? "null" . ")\n");
+    if (!isset($args['path']) || is_null($args['path'])) return [
+        "stderr" => "Error (ash): Missing required field 'path'.",
+        "bytes_written" => 0,
+        "exit_code" => -1
+    ];
+    $path = $args['path'];
     $content = $args['content'] ?? "";
     $append = $args['append'] ?? false;
     $owner = $args['owner'] ?? "";
@@ -10,12 +15,8 @@ $this->functionHandlers['write_file'] = function ($args) {
     echo ("$path\n"); // display just the main argument
     if ($append) $result = file_put_contents($path, $content, FILE_APPEND);
     else $result = file_put_contents($path, $content);
-    if (($owner != "") && ($group != "")) {
-        shell_exec("chown $owner:$group $path");
-    }
-    if ($chmod != "") {
-        shell_exec("chmod $chmod $path");
-    }
+    if (($owner != "") && ($group != "")) shell_exec("chown $owner:$group $path");
+    if ($chmod != "") shell_exec("chmod $chmod $path");
     if ($this->ash->debug) echo ("debug: write_file() bytes written: $result\n");
     $result = ["bytes_written" => $result];
     return $result;
