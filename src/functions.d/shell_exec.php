6f870d0a9b8b4c5ad99a13cc9e5041e1ca9fdcb1
Russell Purinton <russell.purinton@gmail.com>
2023-11-16 22:15:32 -0500

    Update shell_exec.php

--- src/functions.d/shell_exec.php
+++ src/functions.d/shell_exec.php
@@ -3,11 +3,13 @@ $this->functionHandlers['shell_exec'] = function ($args) {
     if ($this->ash->debug) echo ("debug: shell_exec(" . print_r($args, true) . ")\n");
     $command = $args['command'];
     echo ("$ " . $command . "\n"); // display just the main argument
-    $output = shell_exec($command);
+    // redirect stderr to stdout
+    $command .= " 2>&1";
+    exec($command, $output, $exitCode);
+    $output = implode("\n", $output);
     $result = [
-        "stdout" => $output,
-        "stderr" => "",
-        "exitCode" => 0,
+        "stdout+stderr" => $output,
+        "exitCode" => $exitCode,
     ];
     if ($this->ash->debug) echo ("debug: shell_exec() result: " . print_r($result, true) . "\n");
     return $result;
